ac34aee8eddb5bbcd858ab62000fd4963a316f76
Phoeliok <pj.subervie@gmail.com>
2018-08-10 21:18:17 +0200

    Fix

--- main.py
+++ main.py
@@ -73,11 +73,9 @@ try:
         try:
             if sCCS811.available():
                 # Set temperature and humidity from BME680 in order to compensate changes in CCS811 algo.
-                print(db_message[0]['fields']['temperature']) is float)
                 if type(db_message[0]['fields']['temperature']) is float and type(db_message[0]['fields']['humidity']) is float:
                     sCCS811.setEnvironmentalData(db_message[0]['fields']['humidity'], db_message[0]['fields']['temperature'])
-                    print("Set env data OK")
-                    
+  
                 if not sCCS811.readData() and sCCS811.geteCO2() > 0 and sCCS811.geteCO2() < 8192:
                     db_message[0]['fields']['eco2'] = sCCS811.geteCO2()
                     db_message[0]['fields']['tvoc'] = sCCS811.getTVOC()
